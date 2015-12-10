5f9f68a3c714fbdadf79a713841fe9ea1c55dfcd
HechengLi <lihecheng93@gmail.com>
2015-12-09 21:27:30 -0500

    Added number wizard with UI
    Same game with Number Wizard but with menu, buttons, etc.
    Also use random guess instead of binary search

---
+++ NumberWizardwithUI/Assets/NumberWizard.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+	int max;
+	int min;
+	int guess;
+	
+	int MaxGuessesAllowed;
+	
+	public Text text;
+	
+	// Use this for initialization
+	void Start () {
+		StartGame ();
+	}
+	
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		guess = Random.Range (min, max+1);
+		MaxGuessesAllowed = 5;
+		text.text = guess.ToString ();
+	}
+	
+	public void GuessHigher () {
+		min = guess;
+		min++;
+		NextGuess ();
+	}
+	
+	public void GuessLower () {
+		max = guess;
+		max--;
+		NextGuess ();
+	}
+	
+	void NextGuess () {
+		guess = Random.Range (min, max+1);
+		MaxGuessesAllowed--;
+		if (MaxGuessesAllowed <= 0) {
+			Application.LoadLevel ("Win");
+		}
+		text.text = guess.ToString ();
+	}
+}
